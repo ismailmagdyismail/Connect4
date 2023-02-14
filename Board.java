5b1e63b6b77061ea8523ed8b6fb9e538bca0d712
ismail <ismailmagdy88@gmail.com>
2023-02-14 18:57:26 +0200

    updated board Interface

--- Board.java
+++ Board.java
@@ -3,4 +3,6 @@ public interface Board {
     public boolean isDraw();
     public String[][] getGrid();
     public int[] addPiece(int column , String symbol);
+    public boolean isValidColumn(int column );
+    public boolean isValidRow(int row );
 }

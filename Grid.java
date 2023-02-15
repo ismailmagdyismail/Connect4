7935b7b432a912b9ada074e713941ef62ddc0d90
ismail <ismailmagdy88@gmail.com>
2023-02-15 14:10:39 +0200

    Added draw logic

--- Grid.java
+++ Grid.java
@@ -1,10 +1,10 @@
 public class Grid implements Board{
     private static int connectN = 4;
-    int dimensions ;
+    int dimension;
     private String[][] grid ;
-    public Grid(int dimensions )
+    public Grid(int dimension)
     {
-        this.dimensions = dimensions;
+        this.dimension = dimension;
         initializeGrid();
     }
     @Override
@@ -14,7 +14,16 @@ public class Grid implements Board{
 
     @Override
     public boolean isDraw() {
-        return false;
+        int occupiedCells = 0 ;
+        int totalCells = dimension * dimension;
+        for (int i = 0; i< dimension; i++){
+            for (int j = 0; j< dimension; j++){
+                if(!grid[i][j].equals("-")){
+                    occupiedCells++;
+                }
+            }
+        }
+        return occupiedCells == totalCells && !isWinner();
     }
 
     @Override
@@ -29,7 +38,7 @@ public class Grid implements Board{
         }
         int[] cell = new int[2];
         cell[0] = cell[1] = -1;
-        for (int i = dimensions-1 ; i >= 0; i--){
+        for (int i = dimension -1; i >= 0; i--){
             if(grid[i][column].equals("-")){
                 cell[0] = i;
                 cell[1] = column;
@@ -40,22 +49,22 @@ public class Grid implements Board{
         return cell;
     }
     public int getDimension(){
-        return this.dimensions;
+        return this.dimension;
     }
     public boolean isValidColumn(int column){
-        return  column >= 0 && column < dimensions;
+        return  column >= 0 && column < dimension;
     }
     public boolean isValidRow(int row ){
-        return  row >= 0 && row < dimensions;
+        return  row >= 0 && row < dimension;
     }
     private void initializeGrid(){
-        if(dimensions <= connectN){
+        if(dimension <= connectN){
             throw new IndexOutOfBoundsException("Invalid column");
         }
-        this.dimensions = dimensions;
-        grid = new String[dimensions][dimensions];
-        for (int i =0 ;i < dimensions;i++){
-            for (int j= 0;j < dimensions;j++){
+        this.dimension = dimension;
+        grid = new String[dimension][dimension];
+        for (int i = 0; i < dimension; i++){
+            for (int j = 0; j < dimension; j++){
                 grid[i][j] = "-";
             }
         }
@@ -63,9 +72,9 @@ public class Grid implements Board{
     }
 
     private boolean checkColumn() {
-        for(int j = 0 ;j<dimensions;j++){
+        for(int j = 0; j< dimension; j++){
             int connected = 1;
-            for (int i = 1 ; i<dimensions;i++){
+            for (int i = 1; i< dimension; i++){
                 if(grid[i][j].equals(grid[i-1][j]) && !grid[i][j].equals("-") )
                     connected++;
                 else connected = 1;
@@ -76,9 +85,9 @@ public class Grid implements Board{
         return false;
     }
     private boolean checkRow() {
-        for(int i = 0 ;i<dimensions;i++){
+        for(int i = 0; i< dimension; i++){
             int connected = 1;
-            for (int j = 1 ; j<dimensions;j++){
+            for (int j = 1; j< dimension; j++){
                 if(grid[i][j].equals(grid[i][j-1]) && !grid[i][j].equals("-"))
                     connected++;
                 else connected = 1;
@@ -89,8 +98,8 @@ public class Grid implements Board{
         return false;
     }
     private boolean checkLeftDiagonal() {
-        for(int i = 0 ;i<dimensions;i++){
-            for (int j = 0 ; j<dimensions;j++){
+        for(int i = 0; i< dimension; i++){
+            for (int j = 0; j< dimension; j++){
                 int x = i+1 , y = j+1;
                 int connected = 1;
                 while (isValidRow(x) && isValidColumn(y)){
@@ -108,8 +117,8 @@ public class Grid implements Board{
     }
 
     private boolean checkRightDiagonal() {
-        for(int i = 0 ;i<dimensions;i++){
-            for (int j = dimensions-1 ; j>=0;j--){
+        for(int i = 0; i< dimension; i++){
+            for (int j = dimension -1; j>=0; j--){
                 int x = i+1 , y = j-1;
                 int connected = 1;
                 while (isValidRow(x) && isValidColumn(y)){
